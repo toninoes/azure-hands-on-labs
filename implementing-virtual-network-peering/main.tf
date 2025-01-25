08f0a6101c6069cc52a1bdb2ab555192bc795ea2
toninoes <toninoes@gmail.com>
2025-01-25 20:15:09 +0100

    reformat

--- implementing-virtual-network-peering/main.tf
+++ implementing-virtual-network-peering/main.tf
@@ -50,3 +50,30 @@ resource "azurerm_virtual_network_peering" "vnet_1_to_vnet_0" {
   virtual_network_name      = module.vnet_1.name
   remote_virtual_network_id = module.vnet_0.id
 }
+
+# Virtual Machines
+module "vm_0" {
+  source = "git::git@github.com:toninoes/modulodromo.git//azure/virtual_machine"
+
+  admin_username       = var.admin_username
+  name                 = "vm-0"
+  resource_group_name  = var.resource_group_name
+  subnet_name          = "vnet-0-subnet-0"
+  ssh_key_pairs_name   = "${var.ssh_key_pairs_name}-0"
+  virtual_network_name = module.vnet_0.name
+
+  depends_on = [module.vnet_0]
+}
+
+module "vm_1" {
+  source = "git::git@github.com:toninoes/modulodromo.git//azure/virtual_machine"
+
+  admin_username       = var.admin_username
+  name                 = "vm-1"
+  resource_group_name  = var.resource_group_name
+  subnet_name          = "vnet-1-subnet-0"
+  ssh_key_pairs_name   = "${var.ssh_key_pairs_name}-1"
+  virtual_network_name = module.vnet_1.name
+
+  depends_on = [module.vnet_1]
+}
