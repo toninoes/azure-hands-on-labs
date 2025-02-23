1a9ce05adc956e50bbbb3cc85f03e1d185cd3921
toninoes <toninoes@gmail.com>
2025-02-23 12:56:55 +0100

    New app version.

--- containerizing-web-apps-on-azure-app-service/main.tf
+++ containerizing-web-apps-on-azure-app-service/main.tf
@@ -10,7 +10,6 @@ module "gha_access_azure" {
   source = "git::git@github.com:toninoes/modulodromo.git//azure/github_actions_access_to_azure"
 
   app_display_name     = var.app_name
-  github_environment   = "sandbox"
   github_repository    = "azure-hands-on-labs"
   resource_group_name  = var.resource_group_name
   role_definition_name = "Contributor"
--- containerizing-web-apps-on-azure-app-service/outputs.tf
+++ containerizing-web-apps-on-azure-app-service/outputs.tf
@@ -1,1 +1,9 @@
-# Here some outputs if we need, webapp url, etc,..
+output "url_staging" {
+  description = "Url for Staging slot."
+  value       = module.my_app.webapp_slot_url
+}
+
+output "url_production" {
+  description = "Url for Production slot."
+  value       = module.my_app.webapp_url
+}
